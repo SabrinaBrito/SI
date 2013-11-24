5df85d442c4577762464fc2acc3881b562a29129
SabrinaBrito <sabrina.britocc@gmail.com>
2013-11-24 18:19:52 -0200

    adcionando setima vez

--- lab02/todolist/app/models/Task.java
+++ lab02/todolist/app/models/Task.java
@@ -9,27 +9,25 @@ import javax.persistence.*;
 
 @Entity
 public class Task extends Model {
-  
-  @Id
-  public Long id;
-  
-  @Required
-  public String label;
-  
-  public static Finder<Long,Task> find = new Finder(
-		    Long.class, Task.class
-		  );
-  
-  public static List<Task> all() {
-	  return find.all();
+
+	@Id
+	public Long id;
+
+	@Required
+	public String label;
+
+	public static Finder<Long, Task> find = new Finder(Long.class, Task.class);
+
+	public static List<Task> all() {
+		return find.all();
 	}
 
 	public static void create(Task task) {
-	  task.save();
+		task.save();
 	}
 
 	public static void delete(Long id) {
-	  find.ref(id).delete();
+		find.ref(id).delete();
 	}
-    
-}
+
+}	
