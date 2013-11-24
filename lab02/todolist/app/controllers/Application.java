58246ead47da613e6ca150c58a2590647af6bb0b
SabrinaBrito <sabrina.britocc@gmail.com>
2013-11-24 15:57:33 -0200

    adcionando terceira vez

--- lab02/todolist/app/controllers/Application.java
+++ lab02/todolist/app/controllers/Application.java
@@ -8,9 +8,21 @@ import views.html.*;
 
 public class Application extends Controller {
 
-	public static Result index() {
-		  return ok("Hello world!");
-	}
+	  public static Result index() {
+		    return ok(index.render("Your new application is ready."));
+		  }
+		  
+	  public static Result tasks() {
+		    return TODO;
+	  }
+		  
+	  public static Result newTask() {
+		    return TODO;
+	  }
+		  
+	  public static Result deleteTask(Long id) {
+		    return TODO;
+	  }
 
     
 }
